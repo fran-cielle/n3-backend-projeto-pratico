8e8f51886b02d2af9777fac20343e63552b76769
fran-cielle <nasc.francielle@gmail.com>
2019-10-30 20:20:13 -0300

    inserindo as rotas de tarefas e pegar a tarefa pelo id

--- src/app.js
+++ src/app.js
@@ -4,9 +4,11 @@ const app = express();//instancia todas as ferramentas do express
 //roteamento das rotas 
 
 const index = require("./routes/index"); 
+//requisitar o caminho de tarefas
+const tarefas = require("./routes/listaTarefasRoutes"); 
 
 app.use(function (req, res, next){
-    res.header("Access-Control-Allow-Origin", "*");//permite a inda e vinda de todas as informações para todos 
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(//header - "etiquetas" que irão validar e guardar informações 
         "Access-Control-Allow-Header",
         "Origin, X-Request-With, Content-Type, Accept"
@@ -15,6 +17,7 @@ app.use(function (req, res, next){
 });
 
 app.use("/",index);//toda vez que eu acessar a rota / (barra) é para acessar(instanciar) a const index 
+app.use("/tarefas",tarefas);
 
 module.exports = app; //para exportar o arquivo, assim irei conseguir requisitar coisas que existem dentro dela em toda a aplicação
 
--- src/controllers/listaTarefasController.js
+++ src/controllers/listaTarefasController.js
@@ -1,1 +1,11 @@
-const tarefas = require("../model/listaControle.json")
+const tarefas = require("../model/listaControle.json");
+
+exports.get = (req, res) =>{ 
+       res.status(200).send(tarefas);
+}
+
+exports.getById = (req, res) => {
+    const id = req.params.id;
+    const tarefa = tarefas.find(tarefas => tarefas.id == id);
+     res.status(200).send(tarefa);
+}
